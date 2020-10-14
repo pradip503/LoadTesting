79e0f14d100020374dde83847d80bc2740aaacec
Pradip Bhattarai <32413800+pradip503@users.noreply.github.com>
2020-10-14 21:57:14 +0545

    Update index.js
    Added setTimeout again

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/intense', (req, res) => {
 
-  res.send('Hello Intense!');
+  setTimeout(function(){
+        res.send('Hello Intense!');
+    }, 5000);
   
 });
 
